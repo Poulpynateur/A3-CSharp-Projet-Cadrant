5040a62f2a70929189a8d0057a9d5b3522a4a9ab
Sennenryu <troltrol68@hotmail.fr>
2019-12-02 15:27:36 +0100

    Create Unit Test Project

---
+++ EasySaveTests/Controller/ParserTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EasySave.Controller;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave.Controller.Tests
+{
+    [TestClass()]
+    public class ParserTests
+    {
+        [TestMethod()]
+        public void ParseNameTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
