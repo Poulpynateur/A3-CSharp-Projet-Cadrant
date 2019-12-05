82b85701d71e34498fa5de82a5cc99aad236fac2
Guil Diyan <guil.diyan@viacesi.fr>
2019-12-05 16:50:21 +0100

    End CryptoSoft with comments and key argument

---
+++ CryptoSoft/Crypto.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace CryptoSoft
+{
+    class Crypto
+    {
+        /// <summary>
+        /// Member of the class Crypto corresponding to the key used to encrypt and decrypt
+        /// </summary>
+        private string key;
+
+        /// <summary>
+        /// Constructor of the class Crypto. Sets the key
+        /// </summary>
+        /// <param name="password">Key for encrypting and decrypting</param>
+        public Crypto(string password)
+        {
+            this.key = password;
+        }
+
+        /// <summary>
+        /// Method of encrypting a file to another file
+        /// </summary>
+        /// <param name="sourceFile">Path of the source file to be encrypted</param>
+        /// <param name="targetFile">Path of the target file where will be encrypted the data of the sourceFile</param>
+        public void EncryptFile(string sourceFile, string targetFile)
+        {
+            try
+            {
+                //Defines the encoding of the key
+                UnicodeEncoding ue = new UnicodeEncoding();
+                byte[] keyByte = ue.GetBytes(this.key);
+
+                int data;
+
+                //Create a stream for the target file that will be linked to cryptographic transformations to encrypt the sourceFile
+                FileStream fsOut = new FileStream(targetFile, FileMode.Create, FileAccess.Write);
+                RijndaelManaged RMcrypt = new RijndaelManaged();
+                CryptoStream cs = new CryptoStream(fsOut, RMcrypt.CreateEncryptor(keyByte, keyByte), CryptoStreamMode.Write);
+                FileStream fsIn = new FileStream(sourceFile, FileMode.Open, FileAccess.Read);
+
+                //For each byte read, it is written to the targetFile
+                while ((data = fsIn.ReadByte()) != -1)
+                {
+                    cs.WriteByte((byte)data);
+                }
+
+                //Close the streams (Source file, target file and the CryptoStream)
+                fsIn.Close();
+                cs.Close();
+                fsOut.Close();
+            }
+            catch(IOException e)
+            {
+                throw new IOException("IO Error : " + e.Message);
+            }
+            catch(CryptographicException e)
+            {
+                throw new CryptographicException("Cryptographic exception : " + e.Message);
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Error : " + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Method of decrypting a file to another file
+        /// </summary>
+        /// <param name="sourceFile">Path of the source file to be decrypted</param>
+        /// <param name="targetFile">Path of the target file where will be decrypted the data of the sourceFile</param>
+        public void DecryptFile(string sourceFile, string targetFile)
+        {
+            try
+            {
+                //Defines the encoding of the key
+                UnicodeEncoding ue = new UnicodeEncoding();
+                byte[] keyByte = ue.GetBytes(this.key);
+
+                int data;
+
+                //Create a stream for the target file that will be linked to cryptographic transformations to decrypting the sourceFile
+                FileStream fsIn = new FileStream(sourceFile, FileMode.Open);
+                RijndaelManaged RMcrypt = new RijndaelManaged();
+                CryptoStream cs = new CryptoStream(fsIn, RMcrypt.CreateDecryptor(keyByte, keyByte), CryptoStreamMode.Read);
+                FileStream fsOut = new FileStream(targetFile, FileMode.Create);
+
+                //For each byte read, it is written to the targetFile
+                while ((data = cs.ReadByte()) != -1)
+                {
+                    fsOut.WriteByte((byte)data);
+                }
+
+                //Close the streams (Source file, target file and the CryptoStream)
+                fsIn.Close();
+                cs.Close();
+                fsOut.Close();
+            }
+            catch (IOException e)
+            {
+                throw new IOException("IO Error : " + e.Message);
+            }
+            catch (CryptographicException e)
+            {
+                throw new CryptographicException("Cryptographic exception : " + e.Message);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error : " + e.Message);
+            }
+        }
+    }
+}
--- CryptoSoft/Program.cs
+++ CryptoSoft/Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Linq;
 
 namespace CryptoSoft
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Takes some arguments to either encrypt or decrypt a file to another with a given key (with 8 characters)
+        /// </summary>
+        /// <param name="args">A list of arguments : [encrypt/decrypt] [key] [sourcePath] [targetPath]</param>
+        /// <returns>A integer code meaning either it succeed, either it fails or if there hasn't even been an encrypting/decrypting work</returns>
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           //Instanciate the stopwatch
+           Stopwatch chrono = new Stopwatch();
+
+            //Remove the white-spaces characters at the start and at the end of all argument
+            Array.ForEach(args, x => x.Trim());
+
+            //Set the key for the cryptographic work
+            Crypto crypt = new Crypto(args[1]);
+            try
+            {
+                //Start of the stopwatch
+                chrono.Start();
+                
+                //If the first argument is encrypt then it will start the encrypting function and the stopwatch will stop at the end of it
+                if (args[0].Equals("encrypt"))
+                {
+                    crypt.EncryptFile(args[2], args[3]);
+                    chrono.Stop();
+
+                    // Displays and returns the time in ms elapsed by the stopwatch
+                    int time = Convert.ToInt32(chrono.Elapsed.TotalMilliseconds);
+                    Console.WriteLine("Encrypting time :" + time);
+                    return time;
+                }
+                //If the first argument is decrypt then it will start the encrypting function and the stopwatch will stop at the end of it
+                else if (args[0].Equals("decrypt"))
+                {
+                    crypt.DecryptFile(args[2], args[3]);
+                    chrono.Stop();
+
+                    // Displays and returns the time in ms elapsed by the stopwatch
+                    int time = Convert.ToInt32(chrono.Elapsed.TotalMilliseconds);
+                    Console.WriteLine("Decrypting time :" + time);
+                    return time;
+                }
+                //If the first argument isn't any of them it means an error of input and there isn't any encrypting so returns 0
+                else
+                {
+                    Console.WriteLine("Error Input");
+                    return 0;
+                } 
+            }
+            //If an exception is caught, then there has been an error and so returns -1
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
         }
     }
 }
