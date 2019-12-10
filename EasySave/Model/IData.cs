577bc187b305f8128fa77711eacf9dc90870f814
Poulpynateur <nicolas.degheselle@viacesi.fr>
2019-12-10 14:22:38 +0100

    Add a special folder Composants in view

---
+++ EasySave/Model/IData.cs
@@ -0,0 +1,17 @@
+﻿using EasySave.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySave.Model
+{
+    public interface IData
+    {
+        IDisplayable GetDisplayable();
+
+        List<string> GetErpBlacklist();
+        List<string> GetEncryptFormat();
+    }
+}
